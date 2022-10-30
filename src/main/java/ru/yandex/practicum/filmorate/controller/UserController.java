cad80d871b7c87df1844dbfb448712255f71bb1b
Viktor <victorioussword@mail.ru>
2022-10-30 12:49:31 +0400

    30-10-2022  работает Начало разаработки проверк входных данных проходит 33 не проходит 34

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,9 +1,13 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.InputDataException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,36 +15,46 @@ import java.util.List;
 import java.util.Map;
 
 
+
+//@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+
+
     Map<Integer, User> users = new HashMap<>();
     int id = 1;
 
     @GetMapping
     public Map<Integer, User> getUsers() {
+        log.info("GET /users. Количество пользователей: {}",users.size());
         return users;
     }
 
     @PostMapping
-    public User postUser(@RequestBody User user) {
+    public User postUser(@Valid @RequestBody User user) {
         user.setId(id++);
         users.put(user.getId(), user);
+        ageCheck(user);
+        log.info("POST /users. Количество пользователей: {}",users.size());
         return user;
     }
 
     @PutMapping
-    public User putUser(@RequestBody User user) {
+    public User putUser(@Valid @RequestBody User user) {
         users.put(user.getId(), user);
+        log.info("PUT /users. Количество пользователей: {}",users.size());
         return user;
     }
 
 
-    private void checker(User user) {
+    private void ageCheck(User user) {
         LocalDate currentDay = LocalDate.now();
-
         if (currentDay.isBefore(user.getBirthday())) {
-            throw new InputDataException("Указан не корректный возраст");
+             throw new InputDataException("Указан не корректный возраст");
         }
     }
+
+
 }
